1993f8e97151f08ee71c88117abdd2786af0b335
Richard Gill <richardg@stripe.com>
2020-03-25 13:11:36 +0000

    added PORT

--- packages/server/src/index.js
+++ packages/server/src/index.js
@@ -26,7 +26,7 @@ app.post('/payment/start', async (request, response) => {
 app.post('/hooks', handleWebhook)
 app.use('/', express.static('website'))
 
-const PORT = 5000
+const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
   console.log(`Running on port ${PORT}`)
 })
