8a50781af45591783affad889f67377af993cf2b
Richard Gill <richardg@stripe.com>
2020-04-21 10:49:32 +0100

    added webhook signing

--- packages/server/src/webhooks.js
+++ packages/server/src/webhooks.js
@@ -1,11 +1,20 @@
 import { updatePaymentIntent } from './database'
+import stripe from './stripe'
 
 const handlePaymentIntentSucceeded = paymentIntent => {
   return updatePaymentIntent(paymentIntent.id, { succeeded: true })
 }
 
 export const handleWebhook = (request, response) => {
-  const event = request.body
+  let event
+  const stripeSignature = request.headers['stripe-signature']
+  try {
+    event = stripe.webhooks.constructEvent(request.body, stripeSignature, 'whsec_LfZeARGLs3yJFbNP0uEolXeByYei5kTg')
+  } catch (error) {
+    console.error(error)
+    response.status(400).send(`Webhook Error: ${error.message}`)
+    return
+  }
   console.log('/hooks', event)
   switch (event.type) {
     case 'payment_intent.succeeded':
