e7cdfc77b5947ee20c9ffdf5ceb03602a4e5fee1
Richard Gill <richardg@stripe.com>
2020-04-27 15:35:48 +0100

    removed unused css class

---
+++ packages/website/src/components/pages/checkoutJS.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { stripe as stripePromise } from '../../lib/stripe'
+import { apiBase } from '../../lib/api'
+
+export default () => {
+  const onClick = async () => {
+    const response = await fetch(`${apiBase()}/checkout/start`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+
+    const sessionId = (await response.json()).id
+    const stripe = await stripePromise
+    await stripe.redirectToCheckout({
+      sessionId
+    })
+  }
+  return <button onClick={onClick}>Checkout</button>
+}
